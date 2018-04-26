bdecdf27eeeba8bcbea251d337602018070af745
pmzi <pouya.mozafar@gmail.com>
2018-04-26 21:00:43 +0430

    socket connection checker and text for showPresentation page

--- client/bundles/showPresentation/index.js
+++ client/bundles/showPresentation/index.js
@@ -1,7 +1,7 @@
 const io = require('socket.io-client');
 
 class Presentation {
-    
+
     constructor() {
 
         this.presentationUrl = window.presentationUrl;
@@ -16,6 +16,8 @@ class Presentation {
 
     initSocket() {
         this.socket.on('connect', () => {
+
+            this.changeStatus(1); //connected
 
             this.socket.emit('connectToPresentation', this.presentationUrl, (response) => {
                 if (response) {
@@ -33,6 +35,46 @@ class Presentation {
             });
 
         });
+
+        this.socket.on('disconnect', () => {
+            this.changeStatus(0); // disconnect
+        })
+
+    }
+
+    changeStatus(status) {
+        if (status) {
+            // let's show synced
+            $('.socketConnection span').style.transitionDelay = '0.2s'
+            $('.socketConnection span').style.transform = 'scale(1)';
+            $('.socketConnection').style.transform = 'translateX(0px)';
+            setTimeout(() => {
+                // let's hide
+                $('.socketConnection span').style.transitionDelay = '0s'
+                $('.socketConnection').style.transitionDelay = '0.2s'
+                $('.socketConnection span').style.transform = 'scale(0)';
+                $('.socketConnection').style.transform = 'translateX(-100%)';
+            }, 1500)
+
+            // let's make the marker green
+
+            $('.socketConnectionMarker>i').style.backgroundColor = '#27ae60';
+
+            // let's change the text
+
+            $('.socketConnectionTextSliderCont').style.transform = 'translateY(0px)';
+
+        } else {
+
+            // let's make the marker red
+
+            $('.socketConnectionMarker>i').style.backgroundColor = '#c0392b';
+
+            // let's change the text
+
+            $('.socketConnectionTextSliderCont').style.transform = 'translateY(-30px)';
+
+        }
     }
 
 }
