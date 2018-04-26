8c97fb06b6e05a9c7a0a7c3a74bdfdd68d6c77da
pmzi <pouya.mozafar@gmail.com>
2018-04-26 21:14:17 +0430

    user & presentation routes are now divided

--- routes/index.js
+++ routes/index.js
@@ -16,43 +16,22 @@ router.get('/', (req, res, err) => {
 
 });
 
-const setUserRoutes = () => {
+// restrict index for logged in user only
+router.get('/home', auth.mustBeLoggedIn, user.home);
 
-    // restrict index for logged in user only
-    router.get('/home', auth.mustBeLoggedIn, user.home);
+// route to register page
+router.get('/register', auth.mustNotBeLoggedIn, user.register);
 
-    // route to register page
-    router.get('/register', auth.mustNotBeLoggedIn, user.register);
+// route for register action
+router.post('/register', auth.mustNotBeLoggedIn, user.doRegister);
 
-    // route for register action
-    router.post('/register', auth.mustNotBeLoggedIn, user.doRegister);
+// route to login page
+router.get('/login', auth.mustNotBeLoggedIn, user.login);
 
-    // route to login page
-    router.get('/login', auth.mustNotBeLoggedIn, user.login);
+// route for login action
+router.post('/login', auth.mustNotBeLoggedIn, user.doLogin);
 
-    // route for login action
-    router.post('/login', auth.mustNotBeLoggedIn, user.doLogin);
-
-    // route for logout action
-    router.get('/logout', auth.mustBeLoggedIn, user.logout);
-
-    // route for going to presentations page
-    router.get('/presentations', auth.mustBeLoggedIn, user.presentations);
-
-    // route for creating new presentation
-    router.get('/presentations/new', auth.mustBeLoggedIn, user.createPresentation);
-
-    // route for creating new presentation action
-    router.post('/presentations/new', auth.mustBeLoggedIn, presentation.create);
-
-}
-
-const setPresentationRoutes = () => {
-    router.get('/show/:url', presentation.show)
-}
-
-setUserRoutes();
-
-setPresentationRoutes();
+// route for logout action
+router.get('/logout', auth.mustBeLoggedIn, user.logout);
 
 module.exports = router;
--- routes/presentation.js
+++ routes/presentation.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+const user = require('../server/UserController');
 const presentation = require('../server/PresentationController');;
 
 const auth = require('../server/middlewares/auth');
 
+// for the control of the presentation
 router.get("/show/:url/control", auth.mustBeLoggedIn, presentation.control);
+
+// route for going to presentations page
+router.get('/presentations', auth.mustBeLoggedIn, user.presentations);
+
+// route for creating new presentation
+router.get('/presentations/new', auth.mustBeLoggedIn, user.createPresentation);
+
+// route for creating new presentation action
+router.post('/presentations/new', auth.mustBeLoggedIn, presentation.create);
+
+// show the presentation
+router.get('/show/:url', presentation.show)
 
 module.exports = router;
